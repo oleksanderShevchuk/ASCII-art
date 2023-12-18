6befdf67b477318782e7d5cf166782329e974407
oleks <oleks@DESKTOP-7ROI72C>
2023-12-18 20:46:49 +0200

    Add project files.

---
+++ ASCII art/BitmapToASCIIConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCII_art
+{
+    public class BitmapToASCIIConverter
+    {
+        private readonly char[] _asciiTable = { '.', ',', ':', '+', '*', '?', '%', 'S', '#', '@' };
+        private readonly char[] _asciiTableNegative = { '@', '#', 'S', '%', '?', '*', '+', ':', ',', '.' };
+        private readonly Bitmap _bitmap;
+        public BitmapToASCIIConverter(Bitmap bitmap)
+        {
+            _bitmap = bitmap;
+        }
+        public char[][] Convert()
+        {
+            return Convert(_asciiTable);
+        }
+        public char[][] ConvertAsNegative()
+        {
+            return Convert(_asciiTableNegative);
+        }
+        private char[][] Convert(char[] asciiTable)
+        {
+            var result = new char[_bitmap.Height][];
+            for (int i = 0; i < _bitmap.Height; i++)
+            {
+                result[i] = new char[_bitmap.Width];
+                for (int j = 0; j < _bitmap.Width; j++)
+                {
+                    int mapIndex = (int)Map(_bitmap.GetPixel(j, i).R, 0, 0, 255, asciiTable.Length - 1);
+                    result[i][j] = asciiTable[mapIndex];
+                }
+            }
+            return result;
+        }
+        private float Map(float valueToMap, float start1, float start2, float stop1, float stop2)
+        {
+            return ((valueToMap - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+        }
+    }
+}
---
+++ ASCII art/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASCII_art
+{
+    public class Program
+    {
+        private const double WIDTH_OFFSET = 2.2;
+        private const int MAX_WIDTH = 550;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Images | *.bmp; *.png; *.jpg; *.JPEG"
+            };
+            while (true)
+            {
+                Console.ReadLine();
+                if (openFileDialog.ShowDialog() != DialogResult.OK)
+                {
+                    continue;
+                }
+                var bitmap = new Bitmap(openFileDialog.FileName);
+                bitmap = ResizeBitmap(bitmap);
+                bitmap.ToGrayScale();
+
+                var converter = new BitmapToASCIIConverter(bitmap);
+                var rows = converter.Convert();
+
+                foreach ( var row in rows )
+                {
+                    Console.WriteLine(row);
+                }
+
+                var rowAsNegative = converter.ConvertAsNegative();
+                File.WriteAllLines("image.txt", rowAsNegative.Select(r => new string(r)));
+
+                Console.SetCursorPosition(0,0);
+            }
+        }
+        private static Bitmap ResizeBitmap(Bitmap bitmap)
+        {
+            var newHeight = bitmap.Height / WIDTH_OFFSET * MAX_WIDTH / bitmap.Width;
+            if (bitmap.Width > MAX_WIDTH || bitmap.Height > newHeight)
+            {
+                bitmap = new Bitmap(bitmap, new Size(MAX_WIDTH, (int)newHeight));
+            }
+            return bitmap;
+        }
+    }
+}
